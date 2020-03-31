0adfb10eea69f61b5823aa90b62fdc9fdf3326d9
Srini <s@srin.me>
2020-03-31 16:54:34 -0400

    new projectlet - random numbers

---
+++ app/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"gitlab.com/RajaSrinivasan/random/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module gitlab.com/RajaSrinivasan/random
+
+go 1.13
+
+require github.com/spf13/cobra v0.0.7
