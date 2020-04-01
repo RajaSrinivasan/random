5fcb16a150c28b9b2270a7c43d0a71cbc121fa14
Srini <s@srin.me>
2020-04-01 09:53:59 -0400

    series plot support

--- impl/generate/plot.go
+++ impl/generate/plot.go
@@ -3,11 +3,40 @@ package generate
 import (
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 )
 
+func (samples SamplesType) convert() plotter.XYs {
+	pts := make(plotter.XYs, len(samples))
+	for i, _ := range pts {
+		pts[i].X = float64(i)
+		pts[i].Y = samples[i]
+	}
+	return pts
+}
+
 func (samples SamplesType) plotSeries() {
 
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+
+	p.Title.Text = "Random Numbers - Series"
+
+	p.X.Label.Text = "No"
+	p.Y.Label.Text = "Random No"
+
+	err = plotutil.AddLinePoints(p, samples.convert())
+	if err != nil {
+		panic(err)
+	}
+
+	// Save the plot to a PNG file.
+	if err := p.Save(8*vg.Inch, 4*vg.Inch, Output); err != nil {
+		panic(err)
+	}
 }
 
 func (samples SamplesType) plotHistogram() {
